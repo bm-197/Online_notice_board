b1ec9f8a7989132dc40b7f537759f59da88bb60c
bm-197 <bisratmaru111@gmail.com>
2025-01-05 19:28:19 +0300

    update: improved error handling, added /post/{id} endpoint, and secured all endpoint with jwt

--- server/Routes/club.py
+++ server/Routes/club.py
@@ -1,32 +1,73 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Depends, HTTPException
 from ..Models.schemas import Post
 from ..Database.supabase import supabase
+from ..Core.jwt import get_current_user
+from typing import Any
 
 router = APIRouter()
 
 # Endpoint for creating a post by club users, ensuring proper role authorization.
 @router.post("/post")
-def club_post(post: Post, role: str = Header(None)):
-    if role == "club":
-        try:
+def club_create_post(post: Post, data: dict[str, Any] = Depends(get_current_user)):
+    try:
+        if data["role"] == "club":
             supabase.table("posts").insert({
                 "post_title": post.post_title,
                 "post_body": post.post_body,
                 "post_expiray_date": post.post_expiray_date,
-                "posted_by": "club"
+                "posted_by": data["user_id"]
             }).execute()
-            return {"success": "yes"}
-        except Exception as e:
-            return {"success": "no", "error": str(e)}
-    return {"success": "no", "error": "Unauthorized"}
+        
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorized Access")
+    
+    except HTTPException as he:
+        raise he
+        
+    except Exception as e:
+        print(f"error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+    return {"msg": "Post Created Successfully"}
 
 # Endpoint for retrieving all posts for club users, ensuring proper role authorization.
-@router.get("/read")
-def club_read_posts(role: str = Header(None)):
-    if role == "club":
-        try:
-            response = supabase.table("posts").select("*").execute()
-            return {"success": "yes", "data": response.data}
-        except Exception as e:
-            return {"success": "no", "error": str(e)}
-    return {"success": "no", "error": "Unauthorized"}
+@router.get("/post")
+def club_read_posts(data: dict[str, Any] = Depends(get_current_user)):
+    try:
+        if data["role"] == "club":
+            response = supabase.table("posts").select("*").eq("posted_by", data["user_id"]).execute()
+            if not response.data:
+                raise HTTPException(status_code=404, detail="No Posts Found For This Club")
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorized Access")
+    
+    except HTTPException as he:
+        print(f"error: {he.detail}")
+        raise he
+        
+    except Exception as e:
+        print(f"error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+    return response.data
+
+# Endpoint for retrieving a post for clubs, ensuring proper role authorization.
+@router.get("/post/{id}")
+def club_read_post(id: str, data: dict[str, Any] = Depends(get_current_user)):
+    try:
+        if data["role"] == "club":
+            response = supabase.table("posts").select("*").eq("posted_by", data["user_id"]).eq("post_id", id).execute()
+            if not response.data:
+                raise HTTPException(status_code=404, detail=f"No Post Found By {id}")
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorized Access")
+    
+    except HTTPException as he:
+        print(f"error: {he.detail}")
+        raise he
+        
+    except Exception as e:
+        print(f"error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+    return response.data

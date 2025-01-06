8525ff48de650a0e5485ad5f4fc2608ea9dc7d20
bm-197 <bisratmaru111@gmail.com>
2025-01-06 18:58:13 +0300

    update: Modified uiDD

--- pr_ui/ui/src/components/Sidebar.jsx
+++ pr_ui/ui/src/components/Sidebar.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import { List, ListItem, ListItemText, Divider } from "@mui/material";
 import { Link } from "react-router-dom";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import GroupIcon from "@mui/icons-material/Group";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import ReviewIcon from "@mui/icons-material/RateReview";
 
 const Sidebar = () => (
-  <div style={{ width: "250px", height: "100vh", backgroundColor: "#0012", marginTop: "65px"}}>
+  <div
+    style={{
+      width: "250px",
+      backgroundColor: "#fff",
+      height: "100vh",
+      borderRight: "1px solid #e0e0e0",
+      paddingTop: "20px",
+      marginTop: "-5px",
+    }}
+  >
+    <div style={{ padding: "10px 20px", fontWeight: "bold", fontSize: "1.2rem" }}>
+      Online Notice Board
+    </div>
+    <Divider />
     <List>
-      <ListItem>
-        <ListItemText primary="PR Dashboard" />
+      <ListItem button component={Link} to="/dashboard/metrics">
+        <DashboardIcon style={{ marginRight: "10px" }} />
+        <ListItemText primary="Dashboard" />
       </ListItem>
-      <Divider />
       <ListItem button component={Link} to="/dashboard/clubs">
+        <GroupIcon style={{ marginRight: "10px" }} />
         <ListItemText primary="Manage Clubs" />
       </ListItem>
       <ListItem button component={Link} to="/dashboard/posts/review">
+        <ReviewIcon style={{ marginRight: "10px" }} />
         <ListItemText primary="Review Posts" />
       </ListItem>
       <ListItem button component={Link} to="/dashboard/posts/create">
+        <PostAddIcon style={{ marginRight: "10px" }} />
         <ListItemText primary="Create Post" />
       </ListItem>
     </List>
